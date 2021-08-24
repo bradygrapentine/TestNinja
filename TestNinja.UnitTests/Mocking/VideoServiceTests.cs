92022c6a5aa15b10ebab27d0fdaf7b5322005c26
bradygrapentine <brady.grapentine@gmail.com>
2021-08-24 10:01:18 -0400

    added Mock objects via the Moq framework and implemented in test cases

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using TestNinja.Mocking;
 using NUnit.Framework;
+using Moq;
 
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
     public class VideoServiceTests
     {
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FakeFileReader());
+            var mockFileReader = new Mock<IFileReader>(); // mock object via Moq
+            mockFileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+            var service = new VideoService(mockFileReader.Object);
             Assert.That(service.ReadVideoTitle(), Does.Contain("error").IgnoreCase);
         }
     }
